1a69c0c737e8a2f82a8e525c5c2ee56a15c8e0a5
odito <odkolas@gmail.com>
2019-05-29 00:31:39 +0300

    first commit

---
+++ src/components/Cart/CartList.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import CartItem from './CartItem';
+
+export default function CartList(props) {
+    const{value}= props;
+    const {cart,id}=props.value;
+    
+    
+    return (
+        <div className="container-fluid">
+        {
+            cart.map(item=>{
+             return(
+                   < CartItem key={item.id} item={item} value={value} />
+            )
+
+            })
+        } 
+           
+        </div>
+    )
+}
